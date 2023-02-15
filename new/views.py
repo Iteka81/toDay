3883a6e5aee87d1b8bc6ed18b2c28e1033ba3f71
Iteka81 <dbrhitk@gmail.com>
2023-02-15 12:25:22 +0200

    ipwho is template

--- new/views.py
+++ new/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import login
+from . import forms
+import requests
+from django.shortcuts import redirect,render
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+class Signup(View):
+
+    def get(self,request):
+        form = forms.SignupForm()
+        return render(request,'new/signup.html',context={'form':form})
+
+    def post(self,request):
+        form= forms.SignupForm(request.POST)
+        if form.is_valid():
+            user= form.save()
+            login(request,user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+
+@login_required()
+def ninde(request):
+      response= requests.get('https://ipwho.is/fields= ip,timezone.current_time,country,flag.emoji')
+      return render(request,'new/base1.html',{'response':response})
+
+
+class Currencies(View):
+
+    def get(self,request):
+        url = 'https://currency-converter5.p.rapidapi.com/currency/list"'
+        querystring = {"format":"json"}
+        headers = {"X-RapidAPI-KEY": "29a64677b3msh3e760bd6bc4016ap1c0663jsne86061996f08",
+                   "X-RapidAPI-Host":"currency-converter5.p.rapidapi.com"}
+        response= requests.request("GET", url,headers=headers,params=querystring)
+        return render(request, 'new/currencies.html',{'response':response})
--- today/settings.py
+++ today/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'today.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'new.User'
+LOGIN_REDIRECT_URL = 'ninde'
